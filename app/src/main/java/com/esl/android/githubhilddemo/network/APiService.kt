f2e702a0783875d96b15bea41bebff19b8bc872b
Babatunde Okediran <babatunde@esettlementgroup.com>
2021-12-05 06:35:15 +0100

    settings: Setting up architecture

--- app/src/main/java/com/esl/android/githubhilddemo/network/APiService.kt
+++ app/src/main/java/com/esl/android/githubhilddemo/network/APiService.kt
@@ -1,9 +1,11 @@
 package com.esl.android.githubhilddemo.network
 
+import com.esl.android.githubhilddemo.model.GithubDataClass
+import com.esl.android.githubhilddemo.utility.NetworkConstants.URL_REPOSITORIES
 import retrofit2.http.GET
 
 interface APiService {
 
-    @GET
-    fun getData()
+    @GET(URL_REPOSITORIES)
+    suspend fun getPublicRepositories(): GithubDataClass
 }
